5cb5c95fc16b64ff909b7084a785bc6e1855373f
Abhishek Dave <abhidave2000@gmail.com>
2026-06-24 20:56:23 -0400

    fix(Error) Fix the RuboCop error

--- db/migrate/20260625004431_create_users.rb
+++ db/migrate/20260625004431_create_users.rb
@@ -1,13 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[8.1]
   def change
-    create_enum :user_role, ["member", "admin", "guest"] 
+    create_enum :user_role, [ "member", "admin", "guest" ]
 
     create_table :users do |t|
       t.string :name,               null: false
       t.string :email,              null: false
       t.string :password_digest,    null: false
       t.string :avatar_url
-      t.string :role,               enum_type: :user_role, default: "member", null: false    
+      t.string :role,               enum_type: :user_role, default: "member", null: false
       t.string :user_name,          null: false
       t.string :bio
       t.boolean :email_verified,    default: false
@@ -21,6 +21,5 @@ class CreateUsers < ActiveRecord::Migration[8.1]
     add_index :users, :email,    unique: true
     add_index :users, :user_name, unique: true
     add_index :users, :role
-
   end
 end
