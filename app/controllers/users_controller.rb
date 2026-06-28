d359fefa5621cba03c60d419fa693a3884d4a68e
Abhishek Dave <abhidave2000@gmail.com>
2026-06-27 21:30:26 -0400

    feat(:UserController) Add users controller methods

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+
+  def index
+    users = User.where(role: [:manager, :member])
+    render json: { data: users }, status: :success
+  end
+
+  def show
+    render json: { data: @user }, status: :success
+  end
+
+  def create
+    user =  User.create(user_params)
+    if user.persist?
+      render json: { data: user, message: "User Successfully Created" }, status: :success
+    end
+  end
+
+  def update
+    if @user.update(params[:user_params])
+      render json: { data: @user, message: "User Updated Successdfully" }, status: :success
+    else
+      render json: { error: @user.errors.full_messages.join(", "), message: "Can not update the user" }, status: :error
+    end
+  end
+
+  private
+  def set_user
+    @user = User.find_by(params[:id])
+    unless @user return render json: { error: true, message: "Can not find the user" }
+  end
+end
